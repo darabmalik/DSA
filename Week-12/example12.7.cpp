32689d251248ed37ba0fd506dea16907e99c26df
darabmalik <malikdarab@gmail.com>
2025-05-31 01:02:52 +0500

    darab

--- Week-12/example12.7.cpp
+++ Week-12/example12.7.cpp
@@ -13,3 +13,19 @@
 #include <map>
 
 using namespace std;
+
+int set1[] = { 1,2,3 };
+int set2[] = { 1,2,3 };
+vector<int>set3(10);
+
+int main() {
+	vector<int>::iterator first = set3.begin();
+	vector<int>::iterator last = set_union(set1,set1 + 3,set2, set2 + 3, first);
+	while (first != last) {
+		cout << *first << " ";
+		first++;
+	}
+	cout << endl;
+	_getch();
+	return 0;
+}
