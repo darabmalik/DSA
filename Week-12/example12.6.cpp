7adaceae3a016633c67c34dd782536b3e3b4f89b
darabmalik <malikdarab@gmail.com>
2025-05-31 01:01:47 +0500

    darab

--- Week-12/example12.6.cpp
+++ Week-12/example12.6.cpp
@@ -13,3 +13,17 @@
 #include <map>
 
 using namespace std;
+
+const int N = 100;
+
+int main() {
+	vector<int>iv(N);
+	iv[50] = 37;
+	vector<int>::iterator iter = find(iv.begin(), iv.end(), 37);
+	if (iter == iv.end())
+		cout << "Not Found!\n";
+	else
+		cout << "Found at " << iter - iv.begin() << "\n";
+	_getch();
+	return 0;
+}
