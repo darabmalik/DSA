63873ba0b9da8695c154f93b8665d7733501455d
darabmalik <malikdarab@gmail.com>
2025-05-31 01:00:38 +0500

    darab

--- Week-12/example12.5.cpp
+++ Week-12/example12.5.cpp
@@ -13,3 +13,13 @@
 #include <map>
 
 using namespace std;
+int main() {
+	bitset<16>b1("1011011110001011");
+	bitset<16>b2;
+	b2 = ~b1;
+	for (int i = b2.size() - 1; i >= 0; i--)
+		cout << b2[i];
+	cout << endl;
+	_getch();
+	return 0;
+}
