7b1975c4ec2ab17c4461e20b12f8b2dbe7c5559b
darabmalik <malikdarab@gmail.com>
2025-05-31 01:03:45 +0500

    darab

--- Week-12/example12.8.cpp
+++ Week-12/example12.8.cpp
@@ -13,3 +13,19 @@
 #include <map>
 
 using namespace std;
+
+int vec1[10];
+int vec2[10];
+
+int main() {
+	fill(vec1, vec1 + 10, -1);
+	for (int i = 0; i < 10; i++)
+		cout << vec1[i] << " ";
+	cout << endl;
+	fill_n(vec2, 5, -1);
+	for (int j = 0; j < 10; j++)
+		cout << vec2[j] << " ";
+	cout << endl;
+	_getch();
+	return 0;
+}
